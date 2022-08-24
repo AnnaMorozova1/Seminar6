66bd5a4eed48c906e6456b26751f6055ec234125
Anna <anyutka901990@mail.ru>
2022-08-24 14:08:37 +0300

    Zadanie 1

---
+++ contr/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте одномерный массив из 123 случайных чисел. Найдите количество 
+// элементов массива, значения которых лежат в отрезке [10,99].
+
+int [] mass=new int[123];
+int Count=0;
+for(int i = 0; i < mass.Length; i++)
+{
+    mass[i] = new Random().Next(1,123);
+    Console.Write(mass[i]+" ");
+    if (mass[i]>10 && mass[i]<99)
+        {
+         Count=Count+1;
+        }
+}
+Console.WriteLine();
+Console.Write("Колличество элементов в отрезке: ");
+Console.WriteLine(Count);
+
+
